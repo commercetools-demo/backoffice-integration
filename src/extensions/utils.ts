91b0be464fa6e05dcd6c6e4df8d0e47b7024e36e
Behnam Tehrani <behnam777@gmail.com>
2024-02-16 10:30:25 -0500

    Fix injectPageTypeIntoDynamicPage to not return if empty

--- src/extensions/utils.ts
+++ src/extensions/utils.ts
@@ -1,13 +1,15 @@
 import { DynamicPageHandlerResponse } from '../types';
 
 export const injectPageTypeIntoDynamicPage = (dynamicPage: DynamicPageHandlerResponse) => {
-  if (dynamicPage && 'dynamicPageType' in dynamicPage) {
-    return {
-      ...dynamicPage,
-      dataSourcePayload: {
-        pageType: dynamicPage.dynamicPageType,
-      },
-    };
+  if (dynamicPage) {
+    if ('dynamicPageType' in dynamicPage) {
+      return {
+        ...dynamicPage,
+        dataSourcePayload: {
+          pageType: dynamicPage.dynamicPageType,
+        },
+      };
+    }
+    return dynamicPage;
   }
-  return dynamicPage;
 };
