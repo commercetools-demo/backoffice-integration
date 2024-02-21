918f1d5399a2ab7e800f5ef6970c27d75f1ef2f8
Behnam Tehrani <behnam777@gmail.com>
2024-02-21 14:04:21 -0500

    hide studio popup when parameters are missing

--- src/frontend/components/frontend-tastic-wrapper.tsx
+++ src/frontend/components/frontend-tastic-wrapper.tsx
@@ -19,10 +19,14 @@ export const FrontendTasticWrapper = ({ children, data: tastic, className }: Pro
   const { locale } = useParams();
   const customer = process.env.NEXT_PUBLIC_INTEGRATION_FRONTASTIC_CUSTOMER;
   const project = process.env.NEXT_PUBLIC_INTEGRATION_FRONTASTIC_PROJECT;
+  const tasticId = tastic?.tasticId;
+  const pageFolderId = pageData?.pageFolder?.pageFolderId;
+  const pageType = pageData?.data?.dataSources?.['__master']?.pageType;
+  const pageId = pageData?.page?.pageId;
 
   const getUrl = useCallback(
     (env: string) => {
-      return `https://${customer}.frontastic.io/dynamicPages/page?environment=${env}&project=${project}&locale=${locale}&page=${pageData?.page?.pageId}&tastic=${tastic?.tasticId}&node=${pageData?.pageFolder?.pageFolderId}&type=${pageData?.data?.dataSources['__master'].pageType}`;
+      return `https://${customer}.frontastic.io/dynamicPages/page?environment=${env}&project=${project}&locale=${locale}&page=${pageId}&tastic=${tasticId}&node=${pageFolderId}&type=${pageType}`;
     },
     [locale, pageData, tastic],
   );
@@ -35,7 +39,14 @@ export const FrontendTasticWrapper = ({ children, data: tastic, className }: Pro
       'commercetools/ui/products/other-products',
       'commercetools/ui/products/details',
     ].includes(tastic?.tasticType || '');
-  }, []);
+  }, [tastic?.tasticType]);
+
+  const isFrontendAvailable = useMemo(() => {
+    if (!customer || !project || !tasticId || !pageFolderId || !pageType || !pageId) {
+      return false;
+    }
+    return true;
+  }, [customer, project, tasticId, pageFolderId, pageType, pageId]);
 
   return (
     <div
@@ -47,38 +58,42 @@ export const FrontendTasticWrapper = ({ children, data: tastic, className }: Pro
       {isDisplayed && isTooltipDisplayed && (
         <div className="absolute top-10 z-40 px-2 shadow-dark">
           <Popover className="relative p-2 leading-tight">
-            <Popover.Button>
-              <Cog6ToothIcon className="h-15 w-15 bg-white" />
-            </Popover.Button>
+            {(isMerchantCenterAvailable || isFrontendAvailable) && (
+              <Popover.Button>
+                <Cog6ToothIcon className="h-15 w-15 bg-white" />
+              </Popover.Button>
+            )}
 
             <Popover.Panel className="absolute z-10 h-auto w-fit bg-white">
-              <div className="flex flex-row items-center text-sm">
-                Studio
-                <a
-                  href={getUrl('development')}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="w-fit bg-white p-2 text-sm"
-                >
-                  <BugAntIcon className="h-15 w-15 bg-white" />
-                </a>
-                <a
-                  href={getUrl('staging')}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="w-fit bg-white p-2 text-sm"
-                >
-                  <BeakerIcon className="h-15 w-15 bg-white" />
-                </a>
-                <a
-                  href={getUrl('production')}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="w-fit bg-white p-2 text-sm"
-                >
-                  <GlobeAltIcon className="h-15 w-15 bg-white" />
-                </a>
-              </div>
+              {isFrontendAvailable && (
+                <div className="flex flex-row items-center text-sm">
+                  Studio
+                  <a
+                    href={getUrl('development')}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-fit bg-white p-2 text-sm"
+                  >
+                    <BugAntIcon className="h-15 w-15 bg-white" />
+                  </a>
+                  <a
+                    href={getUrl('staging')}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-fit bg-white p-2 text-sm"
+                  >
+                    <BeakerIcon className="h-15 w-15 bg-white" />
+                  </a>
+                  <a
+                    href={getUrl('production')}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-fit bg-white p-2 text-sm"
+                  >
+                    <GlobeAltIcon className="h-15 w-15 bg-white" />
+                  </a>
+                </div>
+              )}
               {isMerchantCenterAvailable && (
                 <div className="grid grid-cols-2 bg-white">
                   <button
